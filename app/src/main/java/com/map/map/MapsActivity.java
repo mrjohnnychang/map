585d4db54450cdf431f4436f1109ae7d4da9d409
Johnny <mrjohnnychang@gmail.com>
2017-05-17 00:02:30 -0700

    5/16

--- app/src/main/java/com/map/map/MapsActivity.java
+++ app/src/main/java/com/map/map/MapsActivity.java
@@ -1,16 +1,22 @@
 package com.map.map;
 
+import android.*;
+import android.Manifest;
+import android.graphics.drawable.Drawable;
 import android.support.v4.app.FragmentActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 
-import com.google.android.gms.maps.CameraUpdate;
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.GoogleMapOptions;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.*;
+import com.google.android.gms.maps.model.*;
+import com.google.maps.android.ui.IconGenerator;
+
+
+
+
+
+
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
@@ -31,7 +37,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
      * Manipulates the map once available.
      * This callback is triggered when the map is ready to be used.
      * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
+     * we just add a marker within SFO
      * If Google Play services is not installed on the device, the user will be prompted to install
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
@@ -41,15 +47,32 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         //Add a marker in SFO and move the camera
-        LatLng sfo = new LatLng(37.6213,-122.3790);
-        mMap.addMarker(new MarkerOptions().position(sfo).title("Marker in SFO"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sfo));
+        LatLng sfo = new LatLng(37.617104, -122.381094);
+        mMap.addMarker(new MarkerOptions()
+                .position(sfo)
+                .title("SFO T2"));
+
+        //set default zoom to SFO zoom level 19
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sfo,19));
+
         GoogleMapOptions options = new GoogleMapOptions();
+
         options.compassEnabled(true)
                 .rotateGesturesEnabled(true)
-                .tiltGesturesEnabled(true)
+                .tiltGesturesEnabled(false)
                 .scrollGesturesEnabled(true)
                 .zoomControlsEnabled(true)
                 .zoomGesturesEnabled(true);
+
+
+
+        /*
+        mMap.addMarker(new MarkerOptions()
+                .position(new LatLng(37.616724,-122.381060))
+                .title("Gate 52")
+                .snippet("Gate 52 text"));
+*/
+
     }
+
 }
